ec14a1686ca047786130b1317e9d244f860c2edf
Yttehs <shetty.yttehs@outlook.com>
2025-02-11 14:50:10 +0800

    feat: impl clickable title

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Platform, Pressable, StyleSheet, Text, useColorScheme, View } from "react-native";
 import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 import { Stack } from "expo-router";
 import Effect from "@/components/Effect";
+import * as Linking from "expo-linking";
 
+const url = "https://resume.shettydev.com";
 const defaultColor = "#B39DDB";
 const title = "Shetty's Resume";
 const content = "TODO";
@@ -11,13 +13,32 @@ export default function Index() {
   const colorScheme = useColorScheme() || "light";
   const { theme } = useMaterial3Theme({ fallbackSourceColor: defaultColor });
 
+  const openResume = () => {
+    if (Platform.OS === "web") {
+      window.location.href = url;
+    } else {
+      Linking.openURL(url);
+    }
+  };
+
   return (
     <>
       <Stack.Screen options={{
           headerStyle: [{
             backgroundColor: theme[colorScheme].primaryContainer,
           }],
-          headerTitle: title,
+          headerTitle() {
+            return (
+              <Pressable onPress={ openResume }>
+                <Text style={[
+                  { color: theme[colorScheme].onPrimaryContainer },
+                  styles.headerTitle,
+                ]}>
+                  {title}
+                </Text>
+              </Pressable>
+            )
+          },
           headerTitleStyle: [
             { color: theme[colorScheme].onPrimaryContainer },
             styles.headerTitle,
