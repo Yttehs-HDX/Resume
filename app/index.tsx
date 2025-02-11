3a4e2caea7b60347d06e14297fc7d603b308382e
Yttehs <shetty.yttehs@outlook.com>
2025-02-11 13:04:54 +0800

    feat: impl material3 theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
+import { Stack } from "expo-router";
+
+const defaultColor = "#B39DDB";
+const title = "Shetty's Resume";
+const content = "TODO";
 
 export default function Index() {
+  const colorScheme = useColorScheme() || "light";
+  const { theme } = useMaterial3Theme({ fallbackSourceColor: defaultColor });
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <Stack.Screen options={{
+          headerStyle: [{
+            backgroundColor: theme[colorScheme].primaryContainer,
+          }],
+          headerTitle: title,
+          headerTitleStyle: [
+            { color: theme[colorScheme].onPrimaryContainer },
+            styles.headerTitle,
+          ],
+          headerShadowVisible: false,
+        }} />
+      <View style={[
+        { backgroundColor: theme[colorScheme].secondaryContainer },
+        styles.contentContainer
+      ]}>
+        <Text style={{ color: theme[colorScheme].onSecondaryContainer }}>
+          {content}
+        </Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontFamily: "cursive",
+    fontWeight: "bold",
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 16,
+  },
+});
