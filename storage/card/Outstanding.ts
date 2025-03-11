ec47ba41663f0de2da55382e95c33865f972ad36
Yttehs <shetty.yttehs@outlook.com>
2025-03-11 16:22:10 +0800

    chore: use list and join function

--- storage/card/Outstanding.ts
+++ storage/card/Outstanding.ts
@@ -1,11 +1,13 @@
 export class Outstanding {
   static readonly Title = "个人优势";
-  static readonly Content = `0. Github 和 个人博客请点击右上角。
-1. 熟悉 Kotlin 语言，具备使用 Jetpack 组件套件开发高质量 Android 应用的能力，具备使用 Kotlin Multiplatform 开发桌面 + Android 双端应用的能力。
-2. 熟悉 JavaScript 和 TypeScript，具备使用 React Native 框架开发跨平台 App 的能力。
-3. 掌握 Rust 语言进行 no_std 环境下的底层系统开发，具备在 RISC-V64 架构上工作的实战经验，开发过 RISC-V 的小型操作系统。
-4. 熟练运用 Linux 命令行和 Shell 脚本操作，将 GNU/Linux 作为主要的生产环境进行日常开发活动，熟练使用 Vim、Git、Makefile 等各种 Cli 工具，具备在 Linux 平台开发 Python、C 的能力。
-5. 具备跨平台编程语言的广泛知识，包括C、C++、Java 及 Python。
-6. 能够高效地进行多语言编程，适应不同技术栈的项目需求。
-7. 具有深厚的系统编程背景，能够在资源受限的环境中优化性能和稳定性。`;
+  static readonly Content = [
+    `0. Github 和 个人博客请点击右上角。`,
+    `1. 熟悉 Kotlin 语言，具备使用 Jetpack 组件套件开发高质量 Android 应用的能力，具备使用 Kotlin Multiplatform 开发桌面 + Android 双端应用的能力。`,
+    `2. 熟悉 JavaScript 和 TypeScript，具备使用 React Native 框架开发跨平台 App 的能力。`,
+    `3. 掌握 Rust 语言进行 no_std 环境下的底层系统开发，具备在 RISC-V64 架构上工作的实战经验，开发过 RISC-V 的小型操作系统。`,
+    `4. 熟练运用 Linux 命令行和 Shell 脚本操作，将 GNU/Linux 作为主要的生产环境进行日常开发活动，熟练使用 Vim、Git、Makefile 等各种 Cli 工具，具备在 Linux 平台开发 Python、C 的能力。`,
+    `5. 具备跨平台编程语言的广泛知识，包括C、C++、Java 及 Python。`,
+    `6. 能够高效地进行多语言编程，适应不同技术栈的项目需求。`,
+    `7. 具有深厚的系统编程背景，能够在资源受限的环境中优化性能和稳定性。`,
+  ].join("\n");
 }
