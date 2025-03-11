172f8f2020c6067ff4226b4d363c9ef7e2719355
Shetty Yttehs <shetty@shettydev.com>
2025-03-12 02:23:35 +0800

    chore: remove useless info

--- constants/Outstanding.ts
+++ constants/Outstanding.ts
@@ -1,7 +1,6 @@
 export class Outstanding {
   static readonly Title = "个人优势";
   static readonly Content = [
-    `0. Github 和 个人博客请点击右上角。`,
     `1. 熟悉 Kotlin 语言，具备使用 Jetpack 组件套件开发高质量 Android 应用的能力，具备使用 Kotlin Multiplatform 开发桌面 + Android 双端应用的能力。`,
     `2. 熟悉 JavaScript 和 TypeScript，具备使用 React Native 框架开发跨平台 App 的能力。`,
     `3. 掌握 Rust 语言进行 no_std 环境下的底层系统开发，具备在 RISC-V64 架构上工作的实战经验，开发过 RISC-V 的小型操作系统。`,
