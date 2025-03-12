0371932578217e8264cc804611df82a7d38898d8
Yttehs <shetty.yttehs@outlook.com>
2025-03-12 23:31:12 +0800

    chore: add a passage

--- constants/Blog.ts
+++ constants/Blog.ts
@@ -9,5 +9,10 @@ export class Blog {
       Title: "React Native MobX",
       Description: "如何在 React Native 项目中实现 MVVM 架构",
     },
+    {
+      Url: `${BasicInfo.Blog}/2024/12/30/react-native-and-kotlin-multiplatform`,
+      Title: "Comparison of React Native and Kotlin Multiplatform",
+      Description: "My feedback for React Native and Kotlin Multiplatform",
+    },
   ];
 }
