5727af90ace55b074a2b1cdf92332a386025392f
Yttehs <shetty.yttehs@outlook.com>
2025-03-12 12:17:25 +0800

    chore: remove old impls

--- components/card-row/inner/RepoCardMatrix.tsx
+++ components/card-row/inner/RepoCardMatrix.tsx
@@ -1,7 +1,5 @@
-import { StyleSheet, FlatList, View, Dimensions } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Material3Theme } from "@pchmn/expo-material3-theme";
-import RepoCard from "@/components/card/RepoCard";
-import getDeviceType from "@/utils/Device";
 
 type Props = {
   theme: Material3Theme;
@@ -9,26 +7,10 @@ type Props = {
 };
 
 export default function RepoCardMatrix({ theme, colorScheme }: Props) {
-  const gap = 16;
-  const cardWidthMax = 300;
-
   return (
-    <FlatList
-      style={styles.container}
-      data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
-      renderItem={() => (
-        <RepoCard
-          theme={theme}
-          colorScheme={colorScheme}
-          title="Test"
-          content="Test"
-        />
-      )}
-      keyExtractor={(item) => item.toString()}
-      numColumns={Math.floor(Dimensions.get("window").width / cardWidthMax)}
-      columnWrapperStyle={{ gap, justifyContent: "flex-start" }}
-      contentContainerStyle={{ gap, justifyContent: "flex-start" }}
-    />
+    <View style={styles.container}>
+      <Text>Test</Text>
+    </View>
   );
 }
 
