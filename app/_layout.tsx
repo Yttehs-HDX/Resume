f0a156ab765fbf14719f70bf9bdea34fd654636a
Yttehs <shetty.yttehs@outlook.com>
2025-03-12 21:00:40 +0800

    feat: support tab dynamic text

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
+import { BasicInfo } from "@/constants/BasicInfo";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const updateTitleAndFavicon = () => {
+      const isHidden = document.visibilityState === 'hidden';
+      if (isHidden) {
+        document.title = `${BasicInfo.NicknameShort}'s Resume`;
+      } else {
+        document.title = "( ゜-゜)つロ乾杯~";
+      }
+    }
+    updateTitleAndFavicon();
+    document.addEventListener('visibilitychange', updateTitleAndFavicon);
+    return () => {
+      document.removeEventListener('visibilitychange', updateTitleAndFavicon);
+    };
+  }, []);
+
   return <Stack />;
 }
