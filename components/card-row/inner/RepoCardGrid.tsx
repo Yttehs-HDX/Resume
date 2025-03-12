01b2d1d635943285b1c605beb49bfeac280de34b
Yttehs <shetty.yttehs@outlook.com>
2025-03-12 12:38:59 +0800

    feat: use 'FlatGrid'

--- components/card-row/inner/RepoCardGrid.tsx
+++ components/card-row/inner/RepoCardGrid.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Material3Theme } from "@pchmn/expo-material3-theme";
+import RepoCard from "@/components/card/RepoCard";
+import { FlatGrid } from 'react-native-super-grid';
 
 type Props = {
   theme: Material3Theme;
@@ -9,7 +11,13 @@ type Props = {
 export default function RepoCardGrid({ theme, colorScheme }: Props) {
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <FlatGrid
+        itemDimension={300}
+        data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
+        renderItem={({ item }) => (
+          <RepoCard theme={theme} colorScheme={colorScheme} title={item} content={item} />
+        )}
+      />
     </View>
   );
 }
@@ -17,5 +25,7 @@ export default function RepoCardGrid({ theme, colorScheme }: Props) {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
+    gridTemplateColumns: "repeat(auto-fill, minmax(100px, 1fr))",
+    gap: 16,
   },
 });
