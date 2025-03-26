fff740b1073806d30818a786e9ec4db8db3a5145
Yttehs <shetty.yttehs@outlook.com>
2025-03-26 23:49:11 +0800

    feat: impl Internship info

--- constants/Internship.ts
+++ constants/Internship.ts
@@ -10,8 +10,22 @@ export class Internship {
         {
           Title: "主要工作：",
           Content: [
-            "1. 使用 React Native 开发移动 APP ，参与 UI 设计与实现。",
-            "2. 设计 API 交互逻辑，封装抽象层接口。",
+            "1. 作为前端项目主管，指导 React Native Expo 移动 APP 开发。",
+            "2. 参与 UI 设计与实现，处理多平台差异。",
+            "3. 设计 API 交互逻辑，封装抽象层接口。",
+          ].join("\n"),
+        },
+        {
+          Title: "杰出贡献：",
+          Content: [
+            "1. 使用 MobX 重构实现类似 Jetpack Compose 的 MVVM 架构，分离视图与数据处理。",
+            "2. 为团队提供若干个抽象子组件，提高组件的复用性，减少开发成本。",
+          ].join("\n"),
+        },
+        {
+          Title: "项目成果：",
+          Content: [
+            "CalGPT APP 已上架 Google Play 和 App Store。",
           ].join("\n"),
         },
       ],
