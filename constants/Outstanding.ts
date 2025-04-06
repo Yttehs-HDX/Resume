006099472779b68c689c74ba45bbcfc2ba22cb20
Yttehs <shetty.yttehs@outlook.com>
2025-04-06 23:15:48 +0800

    fix: typo

--- constants/Outstanding.ts
+++ constants/Outstanding.ts
@@ -5,7 +5,7 @@ export class Outstanding {
     `2. 熟悉 JavaScript 和 TypeScript，具备使用 React Native 框架开发跨平台 App 的能力。`,
     `3. 掌握 Rust 语言进行 no_std 环境下的底层系统开发，具备在 RISC-V64 架构上工作的实战经验，开发过 RISC-V 的小型操作系统。`,
     `4. 熟练运用 Linux 命令行和 Shell 脚本操作，将 GNU/Linux 作为主要的生产环境进行日常开发活动，熟练使用 Vim、Git、Makefile 等各种 Cli 工具，具备在 Linux 平台开发 Python、C 的能力。`,
-    `5. 具备跨平台编程语言的广泛知识，包括C、C++、Java 及 Python。`,
+    `5. 具备跨平台编程语言的广泛知识，包括 C、C++、Java 及 Python。`,
     `6. 能够高效地进行多语言编程，适应不同技术栈的项目需求。`,
     `7. 具有深厚的系统编程背景，能够在资源受限的环境中优化性能和稳定性。`,
   ].join("\n");
