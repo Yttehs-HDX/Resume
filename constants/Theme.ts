d92bfdb6e4177a45fb355fb715214bd4fce4de14
Yttehs <shetty.yttehs@outlook.com>
2025-05-12 22:53:00 +0800

    feat: add more colors

--- constants/Theme.ts
+++ constants/Theme.ts
@@ -1,8 +1,19 @@
 export class Theme {
   static readonly ColorList = [
-    "#B7BDF8",
-    "#A6DA95",
-    "#F5A97F",
+    "#F4DBD6",
+    "#F0C6C6",
+    "#F5BDE6",
     "#C6A0F6",
+    "#ED8796",
+    "#EE99A0",
+    "#F5A97F",
+    "#EED49F",
+    "#A6DA95",
+    "#8BD5CA",
+    "#91D7E3",
+    "#7DC4E4",
+    "#8AADF4",
+    "#B7BDF8",
+    "#C5CFF5",
   ];
 }
