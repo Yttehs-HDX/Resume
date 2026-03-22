dcb7ffcf39e7d8b7603fad76572698ac26562ad2
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-03-22 14:37:09 +0800

    refactor: tighten resume information architecture

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,4 +1,14 @@
 import { ReactNode } from 'react'
+import {
+  User,
+  GraduationCap,
+  Briefcase,
+  Award,
+  Github as GithubIcon,
+  BookOpen,
+  Code2,
+  GitPullRequest,
+} from '@tamagui/lucide-icons'
 import { Profile } from './profile'
 import { Education } from './education'
 import { Intership } from './intership'
@@ -7,7 +17,6 @@ import { Github } from './github'
 import { Contribution } from './contribution'
 import { Blog } from './blog'
 import { Skill } from './skill'
-import { User, GraduationCap, Briefcase, Award, Github as GithubIcon, BookOpen, Code2, GitPullRequest } from '@tamagui/lucide-icons'
 
 /**
  * Route configuration interface
@@ -26,49 +35,49 @@ export const routes: RouteConfig[] = [
   {
     path: '/',
     element: <Profile />,
-    label: '个人简介',
+    label: '个人定位',
     icon: <User size={20} />,
   },
   {
-    path: '/education',
-    element: <Education />,
-    label: '教育经历',
-    icon: <GraduationCap size={20} />,
-  },
-  {
-    path: '/skill',
-    element: <Skill />,
-    label: '专业技能',
-    icon: <Code2 size={20} />,
+    path: '/github',
+    element: <Github />,
+    label: '代表项目',
+    icon: <GithubIcon size={20} />,
   },
   {
     path: '/intership',
     element: <Intership />,
-    label: '实习经历',
+    label: '工程经历',
     icon: <Briefcase size={20} />,
+  },
+  {
+    path: '/skill',
+    element: <Skill />,
+    label: '能力画像',
+    icon: <Code2 size={20} />,
+  },
+  {
+    path: '/contribution',
+    element: <Contribution />,
+    label: '开源协作',
+    icon: <GitPullRequest size={20} />,
+  },
+  {
+    path: '/blog',
+    element: <Blog />,
+    label: '技术文章',
+    icon: <BookOpen size={20} />,
+  },
+  {
+    path: '/education',
+    element: <Education />,
+    label: '学习经历',
+    icon: <GraduationCap size={20} />,
   },
   {
     path: '/certificate',
     element: <Certificate />,
     label: '资格证书',
     icon: <Award size={20} />,
-  },
-  {
-    path: '/github',
-    element: <Github />,
-    label: '作品展示',
-    icon: <GithubIcon size={20} />,
-  },
-  {
-    path: '/contribution',
-    element: <Contribution />,
-    label: '开源贡献',
-    icon: <GitPullRequest size={20} />,
-  },
-  {
-    path: '/blog',
-    element: <Blog />,
-    label: '博客推荐',
-    icon: <BookOpen size={20} />,
   },
 ]
