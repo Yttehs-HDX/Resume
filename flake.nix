eaf67979c9114d1159507b388922fb3b758498a6
Shetty Yttehs <shetty.yttehs@outlook.com>
2026-04-08 19:40:30 +0800

    feat(flake): update nixpkgs to d96b37bbeb9840f1c0ebfe90585ef5067b69bbb3

--- flake.nix
+++ flake.nix
@@ -8,8 +8,7 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+      let pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default =
           pkgs.mkShell { buildInputs = with pkgs; [ nodejs pnpm gnumake ]; };
